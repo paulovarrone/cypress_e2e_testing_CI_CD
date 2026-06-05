eb590f7a1295d8ff1da8b0cea8dd128931bfb2ce
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-06-04 23:13:10 -0300

    Add validation checks for error messages in invalidInputs command

--- cypress/support/invalid_inputs.js
+++ cypress/support/invalid_inputs.js
@@ -11,4 +11,8 @@ Cypress.Commands.add('invalidInputs', () => {
 Cypress.Commands.add('errorMessages', () => {
     cy.contains('#titleFeedback','Please type a title for the image.').should('be.visible');
     cy.contains('#urlFeedback','Please type a valid URL').should('be.visible'); 
+    
+    
+    cy.get('#imageUrl').should('have.css', 'background-image').and('include', 'dc3545') 
+    cy.get('#title').should('have.css', 'background-image').and('include', 'dc3545') 
 })
