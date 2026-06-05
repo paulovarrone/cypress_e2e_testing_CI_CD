cf0966974adac6bc30ffb1608f46bfa65c261525
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-06-04 23:56:36 -0300

    Refactor caseTest4.cy.js to implement specific test cases for valid inputs, image verification, and page refresh

--- cypress/e2e/caseTest4.cy.js
+++ cypress/e2e/caseTest4.cy.js
@@ -1,5 +1,22 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.entrarNoSite4();
+    
+  })
+
+  it('Colocar inputs validos', () => {
+    cy.validInputs4();
+  })
+
+  it('Verificar imagem', () => {
+    cy.verificarIamgemAdicionada4();
+  })
+
+  it('Verificar refresh da pagina', () => {
+    cy.verificarRefresh4();
+  })
+
+    it('Verificar se a imagem esta após reaload', () => {
+    cy.verificarIamgemAdicionada4();
   })
 })
