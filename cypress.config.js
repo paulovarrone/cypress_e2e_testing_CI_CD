13ce904e8c65adb1d78122163ab59811d9f08ebe
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-06-04 21:22:33 -0300

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+
+  e2e: {
+    baseUrl: 'https://localhost:3000/',
+    redirectionLimit: 100,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    blockHosts: [
+      '*googlesyndication.com',
+      '*google-analytics.com',
+      '*googletagmanager.com',
+      '*googletagservices.com',
+      '*doubleclick.net',
+      '*adservice.google.com',
+      '*facebook.net'
+    ],
+    experimentalRunAllSpecs: true,
+    testIsolation: false,
+    setupNodeEvents(on, config) {
+    },
+  },
+});
