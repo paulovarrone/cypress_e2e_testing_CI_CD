67a10c69172240144946fb70353b50cde4b25f14
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-06-04 23:57:03 -0300

    Rename Cypress commands for consistency and clarity in invalid_inputs.js

--- cypress/support/invalid_inputs.js
+++ cypress/support/invalid_inputs.js
@@ -1,14 +1,14 @@
-Cypress.Commands.add('entrarNoSite', () => {
+Cypress.Commands.add('entrarNoSite1', () => {
   cy.visit('/');
 })
 
-Cypress.Commands.add('invalidInputs', () => {
+Cypress.Commands.add('invalidInputs1', () => {
     cy.get('#title').should('be.empty');
     cy.get('#imageUrl').should('be.empty');
     cy.get('#btnSubmit').click();    
 })
 
-Cypress.Commands.add('errorMessages', () => {
+Cypress.Commands.add('errorMessages1', () => {
     cy.contains('#titleFeedback','Please type a title for the image.').should('be.visible');
     cy.contains('#urlFeedback','Please type a valid URL').should('be.visible'); 
     
