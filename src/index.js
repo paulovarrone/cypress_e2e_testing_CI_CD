90e3c4312d4a8f33cd9e607cc1ae5f26e24a1b5d
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-06-04 21:44:51 -0300

    Refactor index.js to initialize service with default items

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import Controller from "./controller.js";
 
 const view = new View();
 
-// localStorage.removeItem('tdd-ew-db')
-
 const service = new Service({
     tableName: 'tdd-ew-db'
 });
+
+const DEFAULT_ITEMS = [
+    { title: 'AI Alien', imageUrl: './img/ai-alien.jpeg' },
+    { title: 'Predator Night Vision', imageUrl: './img/predator.jpeg' },
+    { title: 'ET Bilu', imageUrl: './img/et-bilu.jpeg' },
+];
+
+await service.seedInitialItems(DEFAULT_ITEMS);
 
 const controller = new Controller({ view, service });
 controller.initialize();
